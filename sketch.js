0f9d7d9bae0ba395c82a63ece2e46c68e29540d9
Qianqianye <qianqian.ye44@gmail.com>
2016-09-13 01:13:24 -0700

    Data structure building
    figure out how to parse columns and rows data via google chart data
    format

--- sketch.js
+++ sketch.js
@@ -1,135 +1,120 @@
-var units = "Widgets";
+google.charts.load("current", {packages:["sankey"]});
+  google.charts.setOnLoadCallback(drawChart);
 
-// var margin = {top: 10, right: 10, bottom: 10, left: 10},
-//     width = 700 - margin.left - margin.right,
-//     height = 300 - margin.top - margin.bottom;
+function uniq(a) {
+    var seen = {};
+    return a.filter(function(item) {
+        return seen.hasOwnProperty(item) ? false : (seen[item] = true);
+    });
+}
 
-var formatNumber = d3.format(",.0f"),    // zero decimal places
-    format = function(d) { return formatNumber(d) + " " + units; },
-    color = d3.scale.category20();
+   function drawChart() {
 
-// append the svg canvas to the page
-// var svg = d3.select("#chart").append("svg")
-//     .attr("width", width + margin.left + margin.right)
-//     .attr("height", height + margin.top + margin.bottom)
-//   .append("g")
-//     .attr("transform", 
-//           "translate(" + margin.left + "," + margin.top + ")");
+   $.get("the-counted-2015.csv", function(csvString) {
+      // transform the CSV string into a 2-dimensional array
+   var arrayData = $.csv.toArrays(csvString, {onParseValue: $.csv.hooks.castToScalar});
+   var dataCSV = new google.visualization.arrayToDataTable(arrayData); // this new DataTable object holds all the data
+      var data = new google.visualization.DataTable();
 
-// Set the sankey diagram properties
-var sankey = d3.sankey()
-    .nodeWidth(36)
-    .nodePadding(40)
-    // .size([width, height]);
-
-var path = sankey.link();
+var NewDataArray = []; //new array which is blank
 
 
+var allGenders = [];
+var allRaces = [];
+var allArmed = [];
+for (var i = 0; i < dataCSV.Tf.length; i++) { //loop of all data in the array
+  allGenders.push(dataCSV.Tf[i].c[3].v);
+  allRaces.push(dataCSV.Tf[i].c[4].v);
+  allArmed.push(dataCSV.Tf[i].c[13].v);
+}
+allGenders = uniq(allGenders);
+allRaces = uniq(allRaces);
+allArmed = uniq(allArmed);
 
-// load the data (using the timelyportfolio csv method)
-d3.csv("sankey.csv", function(error, data) {
-
-// var dataset = []
-// d3.csv("the-counted-2015.csv", function(data) {
-//    dataset = data.map(function(d) { return [ +d["max_i"], +d["min_i"] ]; });
-// });
-// console.log(dataset)
+console.log(allGenders);
+console.log(allRaces);
+console.log(allArmed);
 
 
+for (var genderindex = 0; genderindex < allGenders.length; genderindex++) { 
 
+  for (var raceindex = 0; raceindex < allRaces.length; raceindex++) { 
 
-  //set up graph in same style as original example but empty
-  graph = {"nodes" : [], "links" : []};
+    var nowCount = 0;
+    var nowGender = allGenders[genderindex];
+    var nowRace = allRaces[raceindex];
 
-    data.forEach(function (d) {
-      graph.nodes.push({ "name": d.source });
-      graph.nodes.push({ "name": d.target });
-      graph.links.push({ "source": d.source,
-                         "target": d.target,
-                         "value": +d.value });
-     });
-
-     // return only the distinct / unique nodes
-     graph.nodes = d3.keys(d3.nest()
-       .key(function (d) { return d.name; })
-       .map(graph.nodes));
-
-     // loop through each link replacing the text with its index from node
-     graph.links.forEach(function (d, i) {
-       graph.links[i].source = graph.nodes.indexOf(graph.links[i].source);
-       graph.links[i].target = graph.nodes.indexOf(graph.links[i].target);
-     });
-
-     //now loop through each nodes to make nodes an array of objects
-     // rather than an array of strings
-     graph.nodes.forEach(function (d, i) {
-       graph.nodes[i] = { "name": d };
-     });
-
-  sankey
-    .nodes(graph.nodes)
-    .links(graph.links)
-    .layout(32);
-
-// add in the links
-  var link = svg.append("g").selectAll(".link")
-      .data(graph.links)
-    .enter().append("path")
-      .attr("class", "link")
-      .attr("d", path)
-      .style("stroke-width", function(d) { return Math.max(1, d.dy); })
-      .sort(function(a, b) { return b.dy - a.dy; });
-
-// add the link titles
-  link.append("title")
-        .text(function(d) {
-    		return d.source.name + " → " + 
-                d.target.name + "\n" + format(d.value); });
-
-// add in the nodes
-  var node = svg.append("g").selectAll(".node")
-      .data(graph.nodes)
-    .enter().append("g")
-      .attr("class", "node")
-      .attr("transform", function(d) { 
-		  return "translate(" + d.x + "," + d.y + ")"; })
-    .call(d3.behavior.drag()
-      .origin(function(d) { return d; })
-      .on("dragstart", function() { 
-		  this.parentNode.appendChild(this); })
-      .on("drag", dragmove));
-
-// add the rectangles for the nodes
-  node.append("rect")
-      .attr("height", function(d) { return d.dy; })
-      .attr("width", sankey.nodeWidth())
-      .style("fill", function(d) { 
-		  return d.color = color(d.name.replace(/ .*/, "")); })
-      .style("stroke", function(d) { 
-		  return d3.rgb(d.color).darker(2); })
-    .append("title")
-      .text(function(d) { 
-		  return d.name + "\n" + format(d.value); });
-
-// add in the title for the nodes
-  node.append("text")
-      .attr("x", -6)
-      .attr("y", function(d) { return d.dy / 2; })
-      .attr("dy", ".35em")
-      .attr("text-anchor", "end")
-      .attr("transform", null)
-      .text(function(d) { return d.name; })
-    .filter(function(d) { return d.x < width / 2; })
-      .attr("x", 6 + sankey.nodeWidth())
-      .attr("text-anchor", "start");
-
-// the function for moving the nodes
-  function dragmove(d) {
-    d3.select(this).attr("transform", 
-        "translate(" + d.x + "," + (
-                d.y = Math.max(0, Math.min(height - d.dy, d3.event.y))
-            ) + ")");
-    sankey.relayout();
-    link.attr("d", path);
+    for (var dataIndex = 0; dataIndex < dataCSV.Tf.length; dataIndex++) { //loop of all data in the array
+        if(dataCSV.Tf[dataIndex].c[3].v == nowGender  &&  dataCSV.Tf[dataIndex].c[4].v == nowRace ){
+          nowCount++;
+        }
+    }
+    console.log('in all the data we found this:');
+    console.log(nowGender + ' ' + nowRace + ' ' + nowCount);
   }
+}
 
+for (var raceindex = 0; raceindex < allRaces.length; raceindex++) { 
+
+  for (var armedindex = 0; armedindex < allRaces.length; armedindex++) { 
+
+    var nowCount = 0;
+    var nowRace = allRaces[raceindex];
+    var nowArmed = allArmed[armedindex];
+
+    for (var dataIndex = 0; dataIndex < dataCSV.Tf.length; dataIndex++) { //loop of all data in the array
+        if(dataCSV.Tf[dataIndex].c[4].v == nowRace  &&  dataCSV.Tf[dataIndex].c[13].v == nowArmed ){
+          nowCount++;
+        }
+    }
+    console.log('in all the data we found this:');
+    console.log(nowRace + ' ' + nowArmed + ' ' + nowCount);
+
+  }
+}
+
+// for (var dataIndex = 0; dataIndex < dataCSV.Tf.length; dataIndex++) { //loop of all data in the array
+
+//   var miniArray = [];
+//    miniArray.push(dataCSV.Tf[dataIndex].c[3].v);
+//    miniArray.push(dataCSV.Tf[dataIndex].c[4].v );
+//    // miniArray.push(dataCSV.Tf[dataIndex].c[13].v  );
+//    NewDataArray.push(miniArray.count);
+
+//   NewDataArray.push(miniArray);
+
+//   // console.log(miniArray );
+// };
+
+ // console.log(NewDataArray );
+     data.addColumn('string', 'gender');
+     data.addColumn('string', 'raceethnicity');
+     // data.addColumn('string', 'armed');
+     data.addColumn('number', 'Weight');
+     
+     data.addRows(nowCount);
+
+     var colors = ['#a6cee3', '#b2df8a', '#fb9a99', '#fdbf6f',
+                  '#cab2d6', '#ffff99', '#1f78b4', '#33a02c'];
+
+    var options = {
+      height: 400,
+      sankey: {
+        node: {
+          colors: colors
+        },
+        link: {
+          colorMode: 'gradient',
+          colors: colors
+        }
+      }
+    };
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.Sankey(document.getElementById('sankey_multiple'));
+    chart.draw(data, options);
+
+
+});
+
+ }
